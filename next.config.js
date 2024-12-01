97af6ed46971179d8f23e59543a7a5621beade06
ws1040 <29954753@qq.com>
2024-12-01 23:33:33 +0800

    Improve UI

--- next.config.js
+++ next.config.js
@@ -5,11 +5,10 @@ const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
-};
-
-module.exports = {
   webpack: (config) => {
     config.resolve.extensions.push('.mjs');
     return config;
   },
 };
+
+module.exports = nextConfig;
