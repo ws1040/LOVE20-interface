6efc4f8f3bdc41aa38b189069f2d61e571915613
ws1040 <29954753@qq.com>
2024-12-09 16:58:25 +0800

    test button color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-[#334155] text-primary-foreground hover:bg-[#334155]/90',
+        default: 'bg-[#1e3a8a] text-primary-foreground hover:bg-gray-500/90 disabled:bg-gray-500/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
