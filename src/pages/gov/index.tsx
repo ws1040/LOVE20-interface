3a10f762cf1bd77171586689c2a9f1cb9bf4ec17
ws1040 <29954753@qq.com>
2025-01-24 11:55:00 +0800

    Change the duration of each round to 1 day

--- src/pages/gov/index.tsx
+++ src/pages/gov/index.tsx
@@ -23,6 +23,20 @@ const GovPage = () => {
   const { currentRound: currentVoteRound, error: errorCurrentRound } = useCurrentRound();
   const { token: currentToken } = useContext(TokenContext) || {};
 
+  useEffect(() => {
+    if (currentToken && !currentToken.hasEnded) {
+      // 如果发射未结束，跳转到发射页面
+      router.push(`/launch?symbol=${currentToken.symbol}`);
+    } else if (
+      currentToken &&
+      !currentToken.initialStakeRound &&
+      currentToken.symbol != process.env.NEXT_PUBLIC_FIRST_TOKEN_SYMBOL
+    ) {
+      // 如果还没有人质押，跳转到质押页面
+      router.push(`/gov/stakelp?symbol=${currentToken.symbol}&first=true`);
+    }
+  }, [currentToken]);
+
   // 错误处理
   const { handleContractError } = useHandleContractError();
   useEffect(() => {
@@ -30,17 +44,6 @@ const GovPage = () => {
       handleContractError(errorCurrentRound, 'vote');
     }
   }, [errorCurrentRound]);
-
-  // 如果还没有人质押，跳转到质押页面
-  useEffect(() => {
-    if (
-      currentToken &&
-      !currentToken.initialStakeRound &&
-      currentToken.symbol != process.env.NEXT_PUBLIC_FIRST_TOKEN_SYMBOL
-    ) {
-      router.push(`/gov/stakelp?symbol=${currentToken.symbol}&first=true`);
-    }
-  }, [currentToken]);
 
   return (
     <>
--- src/pages/vote/actions4submit.tsx
+++ src/pages/vote/actions4submit.tsx
@@ -27,11 +27,15 @@ const Actions4SubmitPage = () => {
   const router = useRouter();
   const { token: currentToken } = useContext(TokenContext) || {};
   useEffect(() => {
-    if (
+    if (currentToken && !currentToken.hasEnded) {
+      // 如果发射未结束，跳转到发射页面
+      router.push(`/launch?symbol=${currentToken.symbol}`);
+    } else if (
       currentToken &&
       !currentToken.initialStakeRound &&
       currentToken.symbol != process.env.NEXT_PUBLIC_FIRST_TOKEN_SYMBOL
     ) {
+      // 如果还没有人质押，跳转到质押页面
       router.push(`/gov/stakelp?symbol=${currentToken.symbol}&first=true`);
     }
   }, [currentToken]);
