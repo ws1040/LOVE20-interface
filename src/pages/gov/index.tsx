c97719811f147803c339cafff5bd10d48a9976db
Sean <29954753@qq.com>
2025-05-26 11:18:32 +0800

    fixbug: 治理首页 获取治理票的链接错了

--- src/pages/gov/index.tsx
+++ src/pages/gov/index.tsx
@@ -93,7 +93,7 @@ const GovPage = () => {
               <div className="flex flex-col items-center p-4 mt-4">
                 <div className="text-center mb-4 text-greyscale-500">您当前没有治理票，获取治理票后才能治理</div>
                 <Button className="w-1/2" asChild>
-                  <Link href={`/stake?symbol=${currentToken?.symbol}`}>质押获取治理票</Link>
+                  <Link href={`/gov/stakelp/?symbol=${currentToken?.symbol}`}>质押获取治理票</Link>
                 </Button>
               </div>
             )}
