6cb0c18d257b7f285446a2cd65bcebec9f5217be
ws1040 <ws1040@gmail.com>
2025-08-22 23:30:31 +0800

    引入 sentry

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,17 +1,20 @@
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
+import type { NextPageContext } from 'next';
 
-const CustomErrorComponent = (props) => {
+type ErrorProps = { statusCode: number };
+
+const CustomErrorComponent = (props: ErrorProps) => {
   return <Error statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
+CustomErrorComponent.getInitialProps = async (contextData: NextPageContext): Promise<ErrorProps> => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
 
   // This will contain the status code of the response
-  return Error.getInitialProps(contextData);
+  return (await Error.getInitialProps(contextData)) as ErrorProps;
 };
 
 export default CustomErrorComponent;
--- src/pages/api/sentry-example-api.ts
+++ src/pages/api/sentry-example-api.ts
@@ -1,12 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 // Custom error class for Sentry testing
 class SentryExampleAPIError extends Error {
   constructor(message: string | undefined) {
     super(message);
-    this.name = "SentryExampleAPIError";
+    this.name = 'SentryExampleAPIError';
   }
 }
 // A faulty API route to test Sentry's error monitoring
-export default function handler(_req, res) {
-throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
-res.status(200).json({ name: "John Doe" });
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  throw new SentryExampleAPIError('This error is raised on the backend called by the example page.');
+  res.status(200).json({ name: 'John Doe' });
 }
